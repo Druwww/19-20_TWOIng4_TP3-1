f91acd1f6f1ecdce1b589718e9a6a69bf6885306
Quentin M <quentin.mulliez@edu.ece.fr>
2019-11-14 23:23:16 +0100

    patch passage props to child

--- fcb_lite/src/App.js
+++ fcb_lite/src/App.js
@@ -1,41 +1,67 @@
 import React from 'react';
-import { Button } from 'reactstrap';
 import './App.css';
+import { Button } from 'reactstrap';
 import maData from "./DataPersonnes.js"
 
 class ProfilUser extends React.Component {
+  constructor(props) {
+    console.log(props.numero, maData[props.numero].nom);
+    super(props);
+  }
+
+
+
+  render() {
+    console.log("Render Profil user et state : ", maData[this.props.numero].nom);
+
+    return (
+        <div>
+          <p> Son nom : { maData[this.props.numero].nom}</p>
+        </div>
+    );
+  }
 
 }
-
 
 class Profil extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      nomProfil : maData[0].nom,
-    };
+      numeroProfil : 0,
+    }
+    
+    this.handleClickBouttonClick = this.handleClickBouttonClick.bind(this);
   }
 
-  handleClick(numeroProfil) {
-    this.setState({
-      nomProfil : maData[numeroProfil].nom,
-    });
+  handleClickBouttonClick(i) {
+
+    this.setState(
+      {numeroProfil : i}
+    )
+    
+
   }
 
   renderProfilButton(i) {
     return (
-      <Button outline onClick={() => this.handleClick(i)} color="primary">{maData[i].nom}</Button>
+      <Button className="BoutonProfil"  onClick={() => this.handleClickBouttonClick(i)} color="primary" > {maData[i].nom} </Button>
     );
   }
 
   render() {
+    console.log("Render et Nouveau state : ", this.state.numeroProfil);
     return (
+      <div>
         <div>
           {this.renderProfilButton(0)}
           {this.renderProfilButton(1)}
           {this.renderProfilButton(2)}
         </div>
+        <div>
+          <ProfilUser numero={this.state.numeroProfil}/>
+        </div>
+      </div>
     );
   }
 }
